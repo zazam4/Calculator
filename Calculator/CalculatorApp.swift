09995874ae4ba9d5553ad20b746e7678c3290022
Zubair Azam <zazam4@masonlive.gmu.edu>
2021-05-28 12:08:41 -0400

    Initial Commit

---
+++ Calculator/CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorApp.swift
+//  Calculator
+//
+//  Created by Zubair Azam on 5/28/21.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
